917dfd732b8b4acdb608ae37ea9772af4ef84862
CorbinFerrie <corbin.ferrie@gmail.com>
2022-10-27 17:45:35 -0700

    New
    New

--- SmartCollar/SmartCollar.ino
+++ SmartCollar/SmartCollar.ino
@@ -10,11 +10,21 @@ Adafruit_BME280 bme;
 Adafruit_BME280 bme2;
 
 BLEService tempService("180A"); // BLE LED Service
-BLEByteCharacteristic tempCharacteristic("2A57", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic tempCharacteristic("2A19", BLERead | BLEWrite);
 
+long prevMillis = 0;
+int dogSafeTemp = 0;
 void setup() {
   Serial.begin(9600);
 /* assuming I2C address set is 0x76. If you are using 0x77, update 0x77 as the I2C address  */
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+   if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+  
   if (!bme.begin(0x76)) {
     Serial.println("No BME280 device found!");
     while (1);
@@ -29,7 +39,8 @@ void setup() {
   BLE.setAdvertisedService(tempService);
   tempService.addCharacteristic(tempCharacteristic);
   BLE.addService(tempService);
-
+  tempCharacteristic.writeValue(dogSafeTemp); // set initial value for this characteristic
+  
   BLE.advertise();
 
   Serial.println("Bluetooth® device active, waiting for connections...");
@@ -52,22 +63,29 @@ if (central) {
     // check the battery level every 200ms
     // while the central is connected:
     while (central.connected()) {
-        Serial.print("Temperature in deg F = ");
-        double cel = bme.readTemperature();
-        double temp = (cel*9/5) + 32;
-        Serial.println(temp);
+      long currMillis = millis();
 
-        Serial.print("Temperature in deg F2 = ");
-        double cel2 = bme2.readTemperature();
-        double temp2 = (cel2*9/5) + 32;
-        Serial.println(temp2);
-      /*
-        Serial.print("Pressure in hPa = ");
-        Serial.println(bme.readPressure() / 100.0F);
-      */
-
-        Serial.println();
-        delay(5000);
+      if(currMillis - prevMillis >= 1000){
+        prevMillis = millis();
+        Serial.println("HI");
+      }
+      
+//        Serial.print("Temperature in deg F = ");
+//        double cel = bme.readTemperature();
+//        double temp = (cel*9/5) + 32;
+//        Serial.println(temp);
+//
+//        Serial.print("Temperature in deg F2 = ");
+//        double cel2 = bme2.readTemperature();
+//        double temp2 = (cel2*9/5) + 32;
+//        Serial.println(temp2);
+//      /*
+//        Serial.print("Pressure in hPa = ");
+//        Serial.println(bme.readPressure() / 100.0F);
+//      */
+//
+//        Serial.println();
+//        delay(5000);
     }
     // when the central disconnects, turn off the LED:
     digitalWrite(LED_BUILTIN, LOW);
