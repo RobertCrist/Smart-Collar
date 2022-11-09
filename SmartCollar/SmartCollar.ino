d4391ae197693da47e805e9b982e38f228094ccc
Robert Crist <robertcrist99@gmail.com>
2022-11-09 13:09:01 -0800

    Added BLE testing for smart collar

--- SmartCollar/SmartCollar.ino
+++ SmartCollar/SmartCollar.ino
@@ -10,8 +10,9 @@ Adafruit_BME280 bme;
 Adafruit_BME280 bme2;
 
 BLEService tempService("180A"); // BLE LED Service
-BLEDoubleCharacteristic internalTemp("2A19", BLERead | BLENotify);
-BLEDoubleCharacteristic externalTemp("2A20", BLERead | BLENotify);
+
+BLEIntCharacteristic internalTemp("2A19", BLEWrite | BLENotify);
+BLEIntCharacteristic externalTemp("2A20", BLEWrite | BLENotify);
 
 long prevMillis = 0;
 int dogSafeTemp = 0;
@@ -25,7 +26,6 @@ void setup() {
 /* assuming I2C address set is 0x76. If you are using 0x77, update 0x77 as the I2C address  */
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
-  pinMode(6, OUTPUT);
   Serial.println("Starting");
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
@@ -33,13 +33,13 @@ void setup() {
     while (1);
   }
   
-  if (!bme.begin(0x76)) {
-    Serial.println("No BME280 device found!");
-    while (1);
-  }else if (!bme.begin(0x77)) {
-    Serial.println("No BME280 device found!");
-    while (1);
-  }
+  // if (!bme.begin(0x76)) {
+  //   Serial.println("No BME280 device found!");
+  //   while (1);
+  // }else if (!bme.begin(0x77)) {
+  //   Serial.println("No BME280 device found!");
+  //   while (1);
+  // }
   Serial.println("Starting BLE");
 
   BLE.setLocalName("SmartCollar");
@@ -55,47 +55,33 @@ void setup() {
 
 void loop() {
 
-/* read all the parameters from the BME280 sensors via bme object created*/
-
   BLEDevice central = BLE.central();
+  
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
 
-  //while (central.connected()) {
+    while (central.connected()) {
     long currMillis = millis();
 
-    if(currMillis - prevMillis >= 1000){
-      prevMillis = millis();
-      updateBatteryLevel();
-      
-    }
-
-    if(currMillis % 500 == 0){
-      digitalWrite(6, HIGH);
-    }
-
-    if(currMillis % 500 == 250){
-      digitalWrite(6, LOW);
-    }
-  //}
-  if (central) {
-      Serial.print("Connected to central: ");
-      // print the central's BT address:
-      Serial.println(central.address());
-      // turn on the LED to indicate the connection:
-      digitalWrite(LED_BUILTIN, HIGH);
-
-      // check the battery level every 200ms
-      // while the central is connected:
-      
+      if(currMillis - prevMillis >= 1000){
+        prevMillis = millis();
+        test();
         
-    
-      // when the central disconnects, turn off the LED:
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.print("Disconnected from central: ");
-      Serial.println(central.address());
+      }
+    }  
+  
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
   }
 }
 
-void updateBatteryLevel() {
+void updateTemps() {
   Serial.print("Temperature in deg F = ");
   double cel = bme.readTemperature();
   double temp = (cel*9/5) + 32;
@@ -105,6 +91,12 @@ void updateBatteryLevel() {
   double cel2 = bme2.readTemperature();
   double temp2 = (cel2*9/5) + 32;
   Serial.println(temp2);
+}
+
+void test(){
+  int rand = random(50, 60);
+  Serial.println(rand);
+  internalTemp.writeValue(rand);  
 }
 
 
